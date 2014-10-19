58614e24c126b30d7c87d87d04dd8884629ffff7
timchunght <timchunght@gmail.com>
2014-10-19 05:27:41 -0400

    removed some migration files

--- db/migrate/20141019041010_create_projects.rb
+++ db/migrate/20141019041010_create_projects.rb
@@ -2,7 +2,10 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :title
-
+      t.string :img_url
+      t.string :url
+      t.integer :user_id
+      t.string :short_description
       t.timestamps
     end
   end
