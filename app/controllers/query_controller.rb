99e8eee9aadc58a6667e398449cfe7e83cf14cdf
timchunght <timchunght@gmail.com>
2014-10-19 04:03:00 -0400

    Fixed small bugs

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -6,7 +6,7 @@ class QueryController < ApplicationController
    		@parsed = JSON.parse(@data)
    	else
    		@keyword = params[:query][:keyword].to_s
-   		@url = "http://www.kickstarter.com/projects/search.json?search=&term=" + @keyword.split(" ")[0]
+   		@url = "http://www.kickstarter.com/projects/search.json?search=&term=" + @keyword.split(" ")[0].to_s
     	@data = open(@url, :allow_redirections => :safe).read
    		@parsed = JSON.parse(@data)
   	end
